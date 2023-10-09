37521f093817ee09d6d393ae3d8e40f2378da186
Rubén Varela Figueroa <rvarelaf22@fpcoruna.afundacion.org>
2023-10-09 16:20:59 +0200

    Personaje favorito de Los Simpson

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,12 @@
+<html>
+   <body>
+	<h1>Página de bienvenidas</h1>
+	<?php
+	function dar_bienvenida($nombre){
+	  echo "¡Bienvenido/a, " . $nombre . "!";
+	}
+	
+	dar_bienvenida("Bart");
+	?>
+   </body>
+</html>
