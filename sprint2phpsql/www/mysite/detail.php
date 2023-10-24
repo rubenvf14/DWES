4b0b36881a0f79204b1dcb069479e39161d22199
Rubén Varela Figueroa <rvarelaf22@fpcoruna.afundacion.org>
2023-10-24 14:39:53 +0200

    Tabla con css

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -3,6 +3,17 @@
 ?>
 
 <html>
+<head>
+<style>
+	.t1{
+	color: #00A9FF;
+	}
+	.t2{
+	color: #008000;
+	}
+	
+</style>
+</head>
 <body>
 <?php
 	if(!isset($_GET['id'])){
@@ -12,23 +23,23 @@
 	$query = 'SELECT * FROM tjuegos WHERE id='.$juego_id;
 	$result = mysqli_query($db, $query) or die('Query error');
 	$only_row = mysqli_fetch_array($result);
-	echo '<h1>'.$only_row['nombre'].'</h1>';
+	echo '<h1 class="t1">'.$only_row['nombre'].'</h1>';
 	echo '<img src="'.$only_row['url_imagen'].'"/>';
-	echo '<h3>'.$only_row['ano_creacion'].'</h3>';
-	echo '<h3>'.$only_row['genero'].'</h3>';
+	echo '<h3 class="t1">'.$only_row['ano_creacion'].'</h3>';
+	echo '<h3 class="t1">'.$only_row['genero'].'</h3>';
 	?>
-	<h3>Comentarios:</h3>
+	<h3 class="t1">Comentarios:</h3>
 	<ul>
 	<?php
 	 $query2 = 'SELECT * FROM tcomentarios WHERE juego_id='.$juego_id;
 	 $result2 = mysqli_query($db, $query2) or die('Query error');
 	while ($row = mysqli_fetch_array($result2)){
-		echo '<li>'.$row['comentario']."    ".$row['fecha'].'</li>';
+		echo '<li class="t2">'.$row['comentario']."    ".$row['fecha'].'</li>';
 	}
 	mysqli_close($db);
 	?>
 	</ul>
-	<p>Deja un nuevo comentario:</p>
+	<p class="t1">Deja un nuevo comentario:</p>
 	<form action="/comment.php" method="post">
 		<textarea rows="4" cols="50" name="new_comment"></textarea><br>
 		<input type="hidden" name="juego_id" value="<?php echo $juego_id; ?>">
--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -2,27 +2,45 @@
 	 $db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail');
 ?>
 <html>
+<head>
+<style>
+	h1{
+	color:#00A9FF;
+	}
+
+	.c1{
+	background-color:#00A9FF;
+	color: white;
+	}
+
+	.c2{
+	background-color:#90EE90;
+	color:black;
+	}
+
+</style>
+</head>
 <body>
 	<h1>Conexión establecida</h1>
 	
 	<table border="1" align="justify">
 		<tr>
-			<th>ID</th>
-			<th>NOMBRE</th>
-			<th>IMAGEN</th>
-			<th>AÑO DE CREACIÓN</th>
-			<th>GÉNERO</th>
+			<th class="c1">ID</th>
+			<th class="c1">NOMBRE</th>
+			<th class="c1">IMAGEN</th>
+			<th class="c1">AÑO DE CREACIÓN</th>
+			<th class="c1">GÉNERO</th>
 		</tr>
 	<?php
 	$query = 'SELECT * FROM tjuegos';
 	$result = mysqli_query($db, $query) or die('Query error');
 	while ($row = mysqli_fetch_array($result)){
 			echo '<tr>';
-				echo '<th><a href="/detail.php?id='.$row['id'].'">'.$row['id'].'</a></th>';
-				echo '<th>'.$row['nombre'].'</th>';
-				echo '<th><img style="width:350px; height=250px" src="'.$row['url_imagen'].'"></th>';
-				echo '<th>'.$row['ano_creacion'].'</th>';
-				echo '<th>'.$row['genero'].'</th>';
+				echo '<th class="c2"><a href="/detail.php?id='.$row['id'].'">'.$row['id'].'</a></th>';
+				echo '<th class="c2">'.$row['nombre'].'</th>';
+				echo '<th class="c2"><img style="width:350px; height=250px" src="'.$row['url_imagen'].'"></th>';
+				echo '<th class="c2">'.$row['ano_creacion'].'</th>';
+				echo '<th class="c2">'.$row['genero'].'</th>';
 			echo '</tr>';
 	}
 		echo '</table>';
