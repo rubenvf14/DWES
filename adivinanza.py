2561b0eb188f500e260e87bb10eab1330e8a9970
rubenvf14 <145007165+rubenvf14@users.noreply.github.com>
2023-10-31 08:44:30 +0100

    Creación de las 3 adivinanzas con su puntuación

--- adivinanza.py
+++ adivinanza.py
@@ -1,23 +1,61 @@
 print("Adivina adivinanza, ¿Qué le dice un semáforo a otro que le está mirando?")
+puntuacion = 0
 opciones = {
-'a': "Opción 1: Verde que te quiero verde",
-'b': "Opción 2: No me mires que me sonrojo",
-'c': "Opción 3: Amarillo como los chinos",
+'a1': "a: Verde que te quiero verde",
+'b1': "b: No me mires que me sonrojo",
+'c1': "c: Amarillo como los chinos",
 'op1':'a',
 'op2':'b',
-'op3':'c'
+'op3':'c',
+'a2':'a: un coche',
+'b2':'b: la leche',
+'c2':'c: las uvas',
+'a3':'a: peluche',
+'b3':'b: yo-yo',
+'c3':'c: coche'
 }
 
-print(opciones['a'])
-print(opciones['b'])
-print(opciones['c'])
+print(opciones['a1'])
+print(opciones['b1'])
+print(opciones['c1'])
 
 opcionR = input()
 
 if opcionR == opciones['op2']:
     print("Opción correcta")
+    puntuacion = puntuacion + 10
 else:
     print("Opción incorrecta")
+    puntuacion = puntuacion + 5
         
+print('Si las dejas se pasan, pero para venderlas las pesas')
 
+print(opciones['a2'])
+print(opciones['b2'])
+print(opciones['c2'])
 
+opcionR = input()
+
+if opcionR == opciones['op3']:
+    print("Opción correcta")
+    puntuacion = puntuacion + 10
+else:
+    print("Opción incorrecta")
+    puntuacion = puntuacion + 5
+
+print("¿Qué juguete es el más egoísta?")
+
+print(opciones['a3'])
+print(opciones['b3'])
+print(opciones['c3'])
+
+opcionR = input()
+
+if opcionR == opciones['op2']:
+    print("Opción correcta")
+    puntuacion = puntuacion + 10
+else:
+    print("Opción incorrecta")
+    puntuacion = puntuacion + 5
+
+print("Puntuación: ",puntuacion)
