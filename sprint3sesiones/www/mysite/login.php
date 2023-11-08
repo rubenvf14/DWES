980cc39ebaf6892e05a3947f01fcee1f2d6d7ce6
Rubén Varela <rvarelaf22@fpcoruna.afundacion.org>
2023-11-08 12:38:28 +0100

    Ejercicio 3 de sprint3sesiones

---
+++ sprint3sesiones/www/mysite/login.php
@@ -0,0 +1,23 @@
+<?php
+  $db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail');
+
+  $email_posted = $_POST['email'];
+  $password_posted = $_POST['contraseña'];
+  $password_hasheado =  password_hash($password_posted, PASSWORD_DEFAULT);
+  password_verify($password_introducido, $password_hasheado);
+
+  $query = "SELECT id, contraseña FROM tusuarios WHERE email = '".$email_posted."'";
+  $result = mysqli_query($db, $query) or die('Query error');
+  if (mysqli_num_rows($result) > 0){
+	$only_row = mysqli_fetch_array($result);
+	if($only_row[1] == $password_posted){
+		session_start();
+		$_SESSION['id'] = $only_row[0];
+		header('Location: main.php');
+		} else {
+		 echo '<p>Contraseña incorrecta</p>';
+		}
+		} else {
+		 echo '<p>Usuario no encontrado con ese email</p>';
+		}
+?>
