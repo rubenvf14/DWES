81996e6275485ca186908ff163867ceb2ffec872
Rubén Varela Figueroa <rvarelaf22@fpcoruna.afundacion.org>
2023-11-22 13:41:52 +0100

    Apartado 4 de API REST Django

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def pagina_de_prueba(request):
+	return HttpResponse("<h1>Primera página</h1>");
+
