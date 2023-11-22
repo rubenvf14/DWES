88b132f05acd64ca3027cd109eb9d312849187bd
Rubén Varela Figueroa <rvarelaf22@fpcoruna.afundacion.org>
2023-11-22 14:13:31 +0100

    Primer end point Django

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from .models import Tjuegos
 
 # Create your views here.
 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>Primera página</h1>");
 
+def devolver_juegos(request):
+	lista = Tjuegos.objects.all()
+	respuesta_final = []
+	for fila_sql in lista:
+		diccionario = {}
+		diccionario['id'] = fila_sql.id
+		diccionario['nombre'] = fila_sql.nombre
+		diccionario['imagen'] = fila_sql.url_imagen
+		diccionario['ano_creacion'] = fila_sql.ano_creacion
+		diccionario['genero'] = fila_sql.genero
+		respuesta_final.append(diccionario)
+	return JsonResponse(respuesta_final, safe=False)
